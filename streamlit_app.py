9d61595d5fd0b3bb3584cdf66a91a752e9baed70
Laura.Jimenez <112577718+lauriji@users.noreply.github.com>
2023-07-19 12:25:27 +0200

    Update streamlit_app.py
    adding icons to the menu + Avo toast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,10 @@ import streamlit
 streamlit.title("My parents new healthy dinner")
 
 
-streamlit.header("Breakfast Menu")
+streamlit.header("Breakfast Favourites")
 
 
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smothie")
-streamlit.text("Hard-boiled Free-Range Egg")
+streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smothie")
+streamlit.text("🐔Hard-boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avo-Toast")
